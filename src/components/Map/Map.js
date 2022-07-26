dcf5e9a591ddf2a437cbdac615f264baef30db0a
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-25 21:30:59 -0400

    Set up App to render all components

---
+++ src/components/Map/Map.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Map() {
+
+  return (
+    <h1>Map</h1>
+  )
+}
---
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function PlaceDetails() {
+
+  return (
+    <h1>PlaceDetails</h1>
+  )
+}
