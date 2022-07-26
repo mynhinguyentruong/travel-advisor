6d9bff0c00e796fae524c83865d44baf26192e1f
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-26 13:56:49 -0400

    Design and make each place more informative by providing more information
    Design each card that contain restaurant(places)'s details

--- src/components/PlaceDetails/PlaceDetails.js
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -4,7 +4,11 @@ import LocationOnIcon from '@material-ui/icons/LocationOn'
 import PhoneIcon from '@material-ui/icons/Phone'
 import Rating from '@material-ui/lab/Rating'
 
+import useStyles from './styles'
+
 export default function PlaceDetails({place}) {
+
+  const classes = useStyles()
   
   return (
     <Card>
@@ -13,7 +17,36 @@ export default function PlaceDetails({place}) {
           image={place.photo ? place.photo.images.large.url:'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
         />
         <CardContent>
-            <Typography gutterBottom variant="h5">{place.name}</Typography>
+            <Typography gutterBottom variant="h5"
+            >{place.name}
+            <Box display="flex" justifyContent="space-between">
+              <Typography variant="subtitle1">Price</Typography>
+              <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+            </Box>
+            <Box display="flex" justifyContent="space-between">
+              <Typography variant="subtitle1">Ranking</Typography>
+              <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
+            </Box>
+            {place?.awards?.map((award) => (
+              <Box my={1} display="flex" justifyContent="space-between" alignItems="center">
+                <img src={award.images.small} />
+                <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
+              </Box>
+            ))}
+            {place?.cuisine?.map(({name}) => (
+              <Chip key={name} size="small" label={name} className={classes.chip} />
+            ) )}
+            {place.address && (
+          <Typography gutterBottom variant="body2" color="textSecondary" className={classes.subtitle}>
+            <LocationOnIcon />{place.address}
+          </Typography>
+        )}
+        {place.phone && (
+          <Typography variant="body2" color="textSecondary" className={classes.spacing}>
+            <PhoneIcon /> {place.phone}
+          </Typography>
+        )}
+            </Typography>
         </CardContent>
     </Card>
   )
