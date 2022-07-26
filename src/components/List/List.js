24a445e1020846676302943f090869f0cf89caa3
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-25 22:46:17 -0400

    Setup styling for List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import useStyles from './styles.js';
+
 
 export default function List() {
+
+  const classes = useStyles()
 
   return (
     <h1>List</h1>
