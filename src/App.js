2d991bdbbbe7afd6012b8228c3a667d660c47609
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-26 11:33:08 -0400

    Fix error in axios call
    axios call works now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { CssBaseline, Grid } from '@material-ui/core';
 
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 
+import {getPlacesData} from './api'
+
 function App() {
+
+  const [places, setPlaces] = useState([])
+
+  useEffect(() => {
+    getPlacesData().then((data) => {
+        console.log(data)
+        setPlaces(data)
+    })
+  }, [])
+
   return (
     <>
       <CssBaseline />
--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,21 @@
 import axios from 'axios'
 
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+const url = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+
 const options = {
   params: {
     bl_latitude: '11.847676',
     tr_latitude: '12.838442',
     bl_longitude: '109.095887',
     tr_longitude: '109.149359',
-   
+    // restaurant_tagcategory_standalone: '10591',
+    // restaurant_tagcategory: '10591',
+    // limit: '30',
+    // currency: 'USD',
+    // open_now: 'false',
+    // lunit: 'km',
+    // lang: 'en_US'
   },
   headers: {
     'X-RapidAPI-Key': 'df18c2aef8msh13c1ef183b4c8fbp10a1c5jsnbe8e94c9dd36',
@@ -17,8 +25,9 @@ const options = {
 
 export const getPlacesData = async () => {
   try {
-    const response = await axios.get(URL, options)
-    const { data } = response
+    const response = await axios.get(url, options)
+    const { data: { data } } = response
+    return data
 
   } catch(error) {
     console.log(error)
