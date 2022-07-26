de7ec28e691145e127f89ec52ab6f0fb809e563f
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-26 08:23:56 -0400

    Check if useStyles hook is working
    it works

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,8 +1,18 @@
+
 import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+import Rating from '@material-ui/lab'
+import  LocationOnOutlinedIcon  from '@material-ui/icons/LocationOnOutlined'
+
+import useStyles from './styles'
 
 export default function Map() {
 
+  const classes = useStyles()
+
   return (
-    <h1>Map</h1>
+    <div className={classes.mapContainer}>Map goes here</div>
   )
+  
 }
